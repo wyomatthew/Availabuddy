e309e91f66136948c312ffe1f9ab128d4a15f749
Matthew Pickering <mpick@seas.upenn.edu>
2022-01-07 18:41:55 -0500

    pasted in full auth example to bottom

--- main.js
+++ main.js
@@ -1,60 +1,84 @@
+const PROJECT_NUMBER = '282170708765';
+const API_KEY = 'AIzaSyA3ec3t_lNdoGv_aqqqBNJCyZSfj1umBw4';
+const WEB_CLIENT_KEY = '282170708765-m4s4vcfvqpkt4a5ean74q5q51jg05sa9.apps.googleusercontent.com';
+const DESKTOP_CLIENT_KEY = '282170708765-m4s4vcfvqpkt4a5ean74q5q51jg05sa9.apps.googleusercontent.com';
 const BASE_ENDPOINT = 'https://www.googleapis.com/calendar/v3';
 const SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
 
-fetch('https://accounts.google.com/o/oauth2/v2/auth')
-    .then(response => response.json())
-    .then(data => console.log(data));
+// Enter an API key from the Google API Console:
+//   https://console.developers.google.com/apis/credentials
+var apiKey = 'AIzaSyA3ec3t_lNdoGv_aqqqBNJCyZSfj1umBw4';
 
-/*
- * Create form to request access token from Google's OAuth 2.0 server.
- */
-function oauthSignIn() {
-    // Google's OAuth 2.0 endpoint for requesting an access token
-    var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+// Enter the API Discovery Docs that describes the APIs you want to
+// access. In this example, we are accessing the People API, so we load
+// Discovery Doc found here: https://developers.google.com/people/api/rest/
+var discoveryDocs = ["https://people.googleapis.com/$discovery/rest?version=v1"];
 
-    // Create <form> element to submit parameters to OAuth 2.0 endpoint.
-    var form = document.createElement('form');
-    form.setAttribute('method', 'GET'); // Send as a GET request.
-    form.setAttribute('action', oauth2Endpoint);
+// Enter a client ID for a web application from the Google API Console:
+//   https://console.developers.google.com/apis/credentials?project=_
+// In your API Console project, add a JavaScript origin that corresponds
+//   to the domain where you will be running the script.
+var clientId = '282170708765-m4s4vcfvqpkt4a5ean74q5q51jg05sa9.apps.googleusercontent.com';
 
-    // Parameters to pass to OAuth 2.0 endpoint.
-    var params = {
-        'client_id': CLIENT_ID,
-        'redirect_uri': 'http://127.0.0.1:5500/index.html',
-        'response_type': 'token',
-        'scope': SCOPES,
-        'include_granted_scopes': 'true',
-        'state': 'pass-through value'
-    };
+// Enter one or more authorization scopes. Refer to the documentation for
+// the API or https://developers.google.com/people/v1/how-tos/authorizing
+// for details.
+var scopes = 'profile';
 
-    // Add form parameters as hidden input values.
-    for (var p in params) {
-        var input = document.createElement('input');
-        input.setAttribute('type', 'hidden');
-        input.setAttribute('name', p);
-        input.setAttribute('value', params[p]);
-        form.appendChild(input);
-    }
+var authorizeButton = document.getElementById('authorize-button');
+var signoutButton = document.getElementById('signout-button');
 
-    // Add form to page and submit it to open the OAuth 2.0 endpoint.
-    document.body.appendChild(form);
-    form.submit();
+function handleClientLoad() {
+    // Load the API client and auth2 library
+    gapi.load('client:auth2', initClient);
 }
 
-// oauthSignIn();
-const authBtn = document.getElementById('auth');
-authBtn.addEventListener('click', (el, ev) => {
-    oauthSignIn();
-});
+function initClient() {
+    gapi.client.init({
+        apiKey: apiKey,
+        discoveryDocs: discoveryDocs,
+        clientId: clientId,
+        scope: scopes
+    }).then(function () {
+        // Listen for sign-in state changes.
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
 
-function getFreeBusy() {
-    const endpoint = BASE_ENDPOINT + '/freeBusy';
+        // Handle the initial sign-in state.
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
 
-    const from = document.createElement('form');
-    form.setAttribute('method', 'POST');
-    form.setAttribute('action', endpoint);
+        authorizeButton.onclick = handleAuthClick;
+        signoutButton.onclick = handleSignoutClick;
+    });
+}
 
-    // const params = {
-    //     'timeMin': 
-    // }
+function updateSigninStatus(isSignedIn) {
+    if (isSignedIn) {
+        authorizeButton.style.display = 'none';
+        signoutButton.style.display = 'block';
+        makeApiCall();
+    } else {
+        authorizeButton.style.display = 'block';
+        signoutButton.style.display = 'none';
+    }
+}
+
+function handleAuthClick(event) {
+    gapi.auth2.getAuthInstance().signIn();
+}
+
+function handleSignoutClick(event) {
+    gapi.auth2.getAuthInstance().signOut();
+}
+
+// Load the API and make an API call.  Display the results on the screen.
+function makeApiCall() {
+    gapi.client.people.people.get({
+        'resourceName': 'people/me',
+        'requestMask.includeField': 'person.names'
+    }).then(function (resp) {
+        var p = document.createElement('p');
+        var name = resp.result.names[0].givenName;
+        p.appendChild(document.createTextNode('Hello, ' + name + '!'));
+        document.getElementById('content').appendChild(p);
+    });
 }
