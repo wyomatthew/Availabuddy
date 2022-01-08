d4a54e7a9d915c16d4636a42255f05fe921d52ed
Matthew Pickering <mpick@seas.upenn.edu>
2022-01-07 20:57:06 -0500

    adding array of number elements to table identfying day numbers

--- table.js
+++ table.js
@@ -3,6 +3,12 @@ const table = document.getElementById('calendarTable');
 const oneHourNode = document.createElement('tr');
 const numToWeek = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
 
+// get table of number elements
+const numElements = Array(7);
+for (let i = 0; i < numElements.length; i++) {
+    numElements[i] = document.getElementById(numToWeek[i] + 'num');
+}
+
 for (let i = 0; i < 24; i++) {
     const currRow = oneHourNode.cloneNode(false);
     currRow.setAttribute('id', `${i}`);
