0ad28bfe8af778dfe8bdd63ec1785978d4b945e1
Matthew Pickering <mpick@seas.upenn.edu>
2022-01-08 23:24:46 -0500

    implemented detection of current time and embedded date time information into each cell

--- table.js
+++ table.js
@@ -3,16 +3,136 @@ const table = document.getElementById('calendarTable');
 const oneHourNode = document.createElement('tr');
 const numToWeek = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
 
-function clickEvent() {
+// initialize function to convert integer values to RFC dates
+function convertDateToRFC(year = 1970, month = 1, day = 1, hour = 0, minute = 0, second = 0) {
+    return year.toString() + '-' +
+        month.toString().padStart(2, '0') + '-' +
+        day.toString().padStart(2, '0') + 'T' +
+        hour.toString().padStart(2, '0') + ':' +
+        minute.toString().padStart(2, '0') + ':' +
+        second.toString().padStart(2, '0');
+}
 
+/**
+ * 
+ * @param {String} rfcStr 
+ */
+function convertRFCToDate(rfcStr) {
+    const ret = new Date();
+    ret.setFullYear(
+        parseInt(rfcStr.substring(0, 4)),
+        parseInt(rfcStr.substring(5, 7)) - 1,
+        parseInt(rfcStr.substring(8, 10))
+    );
+    ret.setHours(
+        parseInt(rfcStr.substring(11, 13)),
+        parseInt(rfcStr.substring(14, 16)),
+        parseInt(rfcStr.substring(17, 19))
+    );
+    return ret;
+}
+
+// initialize strating date information on Sunday
+let start = new Date();
+
+// get current day of the week
+const dayOfWeek = start.getDay();
+
+// subtract commensurate number of miliseconds from start
+const MS_IN_DAY = 86400000;
+const MS_IN_YEAR = MS_IN_DAY * 365;
+start = new Date(start.valueOf() - MS_IN_DAY * (dayOfWeek));
+
+// get year month and day of sunday
+let year = start.getFullYear();
+let month = start.getMonth() + 1;
+let day = start.getDate();
+
+start.setFullYear(year, month - 1, day);
+start.setHours(0, 0, 0, 0);
+
+// initialize variable to store user state
+let mouseDown = false;
+
+// initialize set of cells that have been highlighted
+let selected = new Set();
+
+// initialize cell of original click
+let firstClicked;
+
+/**
+ * Handles the action of user clicking mouse down on a tile. The clicked
+ * tile is highlighted, added to selected, and the user is put into mouseDown 
+ * state
+ * 
+ * @param {Event} ev 
+ */
+function onMouseDown(ev) {
+    // if mouse is not already down, record click
+    if (!mouseDown) {
+        // active cell if it is not already activated
+        if (!selected.has(ev.target)) {
+            // activate cell
+            ev.target.style.backgroundColor = 'green';
+            selected.add(ev.target);
+        }
+
+        // configure clicked cell as first clicked
+        firstClicked = ev.target;
+
+        // change state
+        mouseDown = true;
+    }
+}
+
+/**
+ * Handles user hovering over a tile with the mouse. If the user is in mouseDown
+ * mode, then the tile should be highlighted and added to selected
+ * 
+ * @param {Event} ev 
+ */
+function onMouseHover(ev) {
+    // activate tile if we are in mouse down mode
+    if (mouseDown) {
+        ev.target.style.backgroundColor = 'green';
+        selected.add(ev.target);
+    }
+}
+
+/**
+ * Handles user lifting mouse over tile. Modifies state to mouse up
+ * 
+ * @param {Event} ev 
+ */
+function onMouseUp(ev) {
+    mouseDown = false;
+}
+
+/**
+ * Handles user moving mouse out of calendar area. Modified state to mouse up
+ * 
+ * @param {Event} ev 
+ */
+function onMouseTableExit(ev) {
+    mouseDown = false;
 }
 
 // get table of number elements
 const numElements = Array(7);
 for (let i = 0; i < numElements.length; i++) {
-    numElements[i] = document.getElementById(numToWeek[i] + 'num');
+    numElements[i] = document.getElementById(numToWeek[i] + 'Num');
 }
 
+// initialize numbers for numElements
+let dayNum = 0;
+numElements.forEach(el => {
+    // compute current offset from start date
+    el.innerHTML = (new Date(start.valueOf() + (dayNum * MS_IN_DAY))).getDate();
+    dayNum++;
+})
+
+// build date table
+let cellList = new Array();
 for (let i = 0; i < 24; i++) {
     const currRow = oneHourNode.cloneNode(false);
     currRow.setAttribute('id', `${i}`);
@@ -21,11 +141,28 @@ for (let i = 0; i < 24; i++) {
     // append 7 cells to the row
     const cellNode = document.createElement('td');
     for (let j = 0; j < 7; j++) {
+        // create current node and add to current row
         const currCell = cellNode.cloneNode(false);
         currCell.setAttribute('headers', numToWeek[j]);
+
+        // get datetime of current cell
+        const currDate = new Date(start.valueOf() + (j * MS_IN_DAY));
+        currCell.setAttribute('data-datetime', currDate.valueOf());
         currRow.appendChild(currCell);
-        currCell.addEventListener('click', (event) => {
-            event.target.style.backgroundColor = 'red';
-        })
+
+        // add to cell list
+        cellList.push(currCell);
+
+        // add mouse events
+        currCell.addEventListener('mousedown', onMouseDown);
+        currCell.addEventListener('mouseenter', onMouseHover);
     }
 }
+
+
+
+// add mouse up function to table
+table.addEventListener('mouseup', onMouseUp);
+
+// add mouse exit function to table
+table.addEventListener('mouseleave', onMouseTableExit);
